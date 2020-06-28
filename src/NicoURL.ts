d7ce6310598b1d00db9fa32f4557d91f2ad24684
Hanakla <ragg.devpr@gmail.com>
2020-06-28 17:33:52 +0900

    Nsen周りのコードを削除

--- src/NicoURL.ts
+++ src/NicoURL.ts
@@ -3,105 +3,88 @@
 //
 
 export const Auth = {
-    // POST : mail_tel, password
-    LOGIN       : "https://secure.nicovideo.jp/secure/login?site=niconico",
+  // POST : mail_tel, password
+  LOGIN: "https://secure.nicovideo.jp/secure/login?site=niconico",
 
-    // GET
-    LOGOUT      : "https://secure.nicovideo.jp/secure/logout",
+  // GET
+  LOGOUT: "https://secure.nicovideo.jp/secure/logout",
 
-    // GET
-    LOGINTEST   : "http://live.nicovideo.jp/api/getplayerstatus/nsen/vocaloid"
+  // GET
+  LOGINTEST: "http://live.nicovideo.jp/api/getplayerstatus/nsen/vocaloid",
 };
 
 export const Live = {
-    //  GET
-    // /:liveId
-    GET_PLAYER_STATUS: "http://live.nicovideo.jp/api/getplayerstatus/",
+  //  GET
+  // /:liveId
+  GET_PLAYER_STATUS: "http://live.nicovideo.jp/api/getplayerstatus/",
 
-    //  パラメータ: コメントスレッドID
-    GET_POSTKEY: "http://live.nicovideo.jp/api/getpostkey?thread=%s",
-
-    //  パラメータ: 放送ID, 動画ID
-    // GET
-    //   v       : LiveID
-    //   id      : Request movie ID (if request)
-    //   mode    : "cancel", "requesting", or none
-    NSEN_REQUEST: "http://live.nicovideo.jp/api/nsenrequest",
-
-    // GET
-    //   v   : LiveID
-    NSEN_GOOD: "http://ow.live.nicovideo.jp/api/nsengood",
-
-    // GET
-    //   v   : LiveID
-    NSEN_SKIP: "http://ow.live.nicovideo.jp/api/nsenskip"
+  //  パラメータ: コメントスレッドID
+  GET_POSTKEY: "http://live.nicovideo.jp/api/getpostkey?thread=%s",
 };
 
-
 export const Video = {
-    //  GET
-    //  /:movieId
-    GET_VIDEO_INFO: "http://ext.nicovideo.jp/api/getthumbinfo/",
+  //  GET
+  //  /:movieId
+  GET_VIDEO_INFO: "http://ext.nicovideo.jp/api/getthumbinfo/",
 
-    // GET
-    // /:movieId
-    GETFLV : "http://flapi.nicovideo.jp/api/getflv/"
+  // GET
+  // /:movieId
+  GETFLV: "http://flapi.nicovideo.jp/api/getflv/",
 };
 
 export const MyList = {
-    FETCH_TOKEN: "http://www.nicovideo.jp/my/mylist",
-    GET_GROUPS: "http://www.nicovideo.jp/api/mylistgroup/list",
+  FETCH_TOKEN: "http://www.nicovideo.jp/my/mylist",
+  GET_GROUPS: "http://www.nicovideo.jp/api/mylistgroup/list",
 
-    DefList : {
-        LIST: "http://www.nicovideo.jp/api/deflist/list",
+  DefList: {
+    LIST: "http://www.nicovideo.jp/api/deflist/list",
 
-        //  フォームデータ: item_type, item_id, token, ?description
-        ADD: "http://www.nicovideo.jp/api/deflist/add",
+    //  フォームデータ: item_type, item_id, token, ?description
+    ADD: "http://www.nicovideo.jp/api/deflist/add",
 
-        // POST
-        //   id_list[0][]    : MyList Item ID Array for deletion
-        //   token           : MyList control token
-        DELETE: "http://www.nicovideo.jp/api/deflist/delete",
+    // POST
+    //   id_list[0][]    : MyList Item ID Array for deletion
+    //   token           : MyList control token
+    DELETE: "http://www.nicovideo.jp/api/deflist/delete",
 
-        // POST
-        //   target_group_id : MyListID to move
-        //   id_list[0][]    : MyList Item ID Array
-        //   token           : MyList control token
-        MOVE: "http://www.nicovideo.jp/api/deflist/move"
-    },
+    // POST
+    //   target_group_id : MyListID to move
+    //   id_list[0][]    : MyList Item ID Array
+    //   token           : MyList control token
+    MOVE: "http://www.nicovideo.jp/api/deflist/move",
+  },
 
+  Normal: {
+    // パラメータ: マイリストID
+    // GET
+    //   group_id        : MyListID
+    LIST: "http://www.nicovideo.jp/api/mylist/list?group_id=%s",
 
-    Normal  : {
-        // パラメータ: マイリストID
-        // GET
-        //   group_id        : MyListID
-        LIST: "http://www.nicovideo.jp/api/mylist/list?group_id=%s",
+    // POST
+    //   group_id        : MyListID
+    //   item_id         : Movie ID
+    //   description     : comment
+    //   token           : MyList control token
+    ADD: "http://www.nicovideo.jp/api/mylist/add",
 
-        // POST
-        //   group_id        : MyListID
-        //   item_id         : Movie ID
-        //   description     : comment
-        //   token           : MyList control token
-        ADD: "http://www.nicovideo.jp/api/mylist/add",
+    // POST
+    //   group_id        : MyListID
+    //   id_list[0][]    : MyList Item ID Array for deletion
+    //   token           : MyList control token
+    DELETE: "http://www.nicovideo.jp/api/mylist/delete",
 
-        // POST
-        //   group_id        : MyListID
-        //   id_list[0][]    : MyList Item ID Array for deletion
-        //   token           : MyList control token
-        DELETE: "http://www.nicovideo.jp/api/mylist/delete",
-
-        // POST
-        //   group_id        : MyListID from move
-        //   target_group_id : MyListID to move
-        //   id_list[0][]    : MyList Item ID Array
-        //   token           : MyList control token
-        MOVE: "http://www.nicovideo.jp/api/deflist/move"
-    }
+    // POST
+    //   group_id        : MyListID from move
+    //   target_group_id : MyListID to move
+    //   id_list[0][]    : MyList Item ID Array
+    //   token           : MyList control token
+    MOVE: "http://www.nicovideo.jp/api/deflist/move",
+  },
 };
 
 export const User = {
-    // GET
-    //   user_id     : UserID
-    //   __format    : Response format("xml" or "json")
-    INFO : "http://api.ce.nicovideo.jp/api/v1/user.info"
+  // GET
+  //   user_id     : UserID
+  //   __format    : Response format("xml" or "json")
+  INFO: "http://api.ce.nicovideo.jp/api/v1/user.info",
 };
--- src/live/NicoLiveApi.ts
+++ src/live/NicoLiveApi.ts
@@ -1,25 +1,24 @@
-import * as _ from 'lodash'
-import * as Cheerio from 'cheerio'
-import * as deepFreeze from 'deep-freeze'
+import * as _ from "lodash";
+import * as Cheerio from "cheerio";
+import * as deepFreeze from "deep-freeze";
 
-import APIEndpoints from '../APIEndpoints'
-import NicoException from '../NicoException'
-import NicoSession from '../NicoSession'
-import NicoLiveInfo from './NicoLiveInfo'
-// import NsenChannels from './NsenChannels'
-// import NsenChannel from './NsenChannel'
-import {LiveMetaData} from '../Entity/LiveMetaData'
-import {default as LiveSession, NicoLiveConnectPreference} from './LiveSession'
-
+import APIEndpoints from "../APIEndpoints";
+import NicoException from "../NicoException";
+import NicoSession from "../NicoSession";
+import NicoLiveInfo from "./NicoLiveInfo";
+import { LiveMetaData } from "../Entity/LiveMetaData";
+import {
+  default as LiveSession,
+  NicoLiveConnectPreference,
+} from "./LiveSession";
 
 export default class NicoLiveApi {
-    // private _nsenChannelInstances: {[chId: string]: NsenChannel} = {}
-    private _nicoLiveInstances: {[liveId: string]: NicoLiveInfo} = {}
+  private _nicoLiveInstances: { [liveId: string]: NicoLiveInfo } = {};
 
-    /**
-     * @param {NicoSession} _session
-     */
-    constructor(private _session: NicoSession) {}
+  /**
+   * @param {NicoSession} _session
+   */
+  constructor(private _session: NicoSession) {}
 }
 
 /**
@@ -31,115 +30,123 @@ export default class NicoLiveApi {
  * @param {string}   liveId  放送ID
  * @return {Promise}
  */
-export const getLiveInfo = async (session: NicoSession, liveId: string): Promise<LiveMetaData> =>
-{
-    if (typeof liveId !== "string" || liveId === "") {
-        throw new TypeError("liveId must bea string");
-    }
+export const getLiveInfo = async (
+  session: NicoSession,
+  liveId: string
+): Promise<LiveMetaData> => {
+  if (typeof liveId !== "string" || liveId === "") {
+    throw new TypeError("liveId must bea string");
+  }
 
-    // if (this._nicoLiveInstances[liveId]) {
-    //     return this._nicoLiveInstances[liveId]
-    // }
+  // if (this._nicoLiveInstances[liveId]) {
+  //     return this._nicoLiveInstances[liveId]
+  // }
 
-    const res = await APIEndpoints.live.getPlayerStatus(session, {liveId})
-    if (res.statusCode === 503) {
-        throw new NicoException({message: `Live[${liveId}]: Nicovideo has in maintenance.`})
-    }
+  const res = await APIEndpoints.live.getPlayerStatus(session, { liveId });
+  if (res.statusCode === 503) {
+    throw new NicoException({
+      message: `Live[${liveId}]: Nicovideo has in maintenance.`,
+    });
+  }
 
-    return _parseLiveInfoResponse(res.body)
-}
+  return _parseLiveInfoResponse(res.body);
+};
 
-export const getLiveSession = async (session: NicoSession, liveId: LiveMetaData|string, options: Partial<NicoLiveConnectPreference> = {}): Promise<LiveSession> =>
-{
-    const liveMeta: LiveMetaData = typeof liveId === 'string' ? await getLiveInfo(session, liveId) : liveId
-    const liveSession = new LiveSession(session, liveMeta)
+export const getLiveSession = async (
+  session: NicoSession,
+  liveId: LiveMetaData | string,
+  options: Partial<NicoLiveConnectPreference> = {}
+): Promise<LiveSession> => {
+  const liveMeta: LiveMetaData =
+    typeof liveId === "string" ? await getLiveInfo(session, liveId) : liveId;
+  const liveSession = new LiveSession(session, liveMeta);
 
-    if (options.connect) {
-        await liveSession.connect(options)
-    }
+  if (options.connect) {
+    await liveSession.connect(options);
+  }
 
-    return liveSession
-}
+  return liveSession;
+};
 
-const _parseLiveInfoResponse = (responseBody: string): LiveMetaData =>
-{
-    const $res    = Cheerio.load(res)
-    const $root   = $res(":root")
-    const $stream = $res("stream")
-    const $user   = $res("user")
-    const $rtmp   = $res("rtmp")
-    const $ms     = $res("ms")
+const _parseLiveInfoResponse = (responseBody: string): LiveMetaData => {
+  const $res = Cheerio.load(res);
+  const $root = $res(":root");
+  const $stream = $res("stream");
+  const $user = $res("user");
+  const $rtmp = $res("rtmp");
+  const $ms = $res("ms");
 
-    if ($root.attr("status") !== "ok") {
-        const errorCode = $res("error code").text()
-        throw new NicoException({
-            message: `Failed to parse live info (${errorCode})`,
-            code : +errorCode,
-            response : responseBody,
-        })
-    }
+  if ($root.attr("status") !== "ok") {
+    const errorCode = $res("error code").text();
+    throw new NicoException({
+      message: `Failed to parse live info (${errorCode})`,
+      code: +errorCode,
+      response: responseBody,
+    });
+  }
 
-    const props: LiveMetaData = deepFreeze({
-        stream  : {
-            liveId      : $stream.find("id").text(),
-            title       : $stream.find("title").text(),
-            description : $stream.find("description").text(),
+  const props: LiveMetaData = deepFreeze({
+    stream: {
+      liveId: $stream.find("id").text(),
+      title: $stream.find("title").text(),
+      description: $stream.find("description").text(),
 
-            watchCount  : $stream.find("watch_count").text()|0,
-            commentCount: $stream.find("comment_count")|0,
+      watchCount: $stream.find("watch_count").text() | 0,
+      commentCount: $stream.find("comment_count") | 0,
 
-            baseTime    : new Date(($stream.find("base_time").text()|0) * 1000),
-            openTime    : new Date(($stream.find("open_time").text()|0) * 1000),
-            startTime   : new Date(($stream.find("start_time").text()|0) * 1000),
-            endTime     : new Date(($stream.find("end_time")|0) * 1000),
+      baseTime: new Date(($stream.find("base_time").text() | 0) * 1000),
+      openTime: new Date(($stream.find("open_time").text() | 0) * 1000),
+      startTime: new Date(($stream.find("start_time").text() | 0) * 1000),
+      endTime: new Date(($stream.find("end_time") | 0) * 1000),
 
-            isOfficial  : $stream.find("provider_type").text() === "official",
-            // isNsen      : $res("ns").length > 0,
+      isOfficial: $stream.find("provider_type").text() === "official",
 
-            contents    : _.map($stream.find("contents_list contents"), function(el) {
-                let left, left1
-                const $content = Cheerio(el)
-                return {
-                    id              : $content.attr("id"),
-                    startTime       : new Date(($content.attr("start_time")|0) * 1000),
-                    disableAudio    : ($content.attr("disableAudio")|0) === 1,
-                    disableVideo    : ($content.attr("disableVideo")|0) === 1,
-                    duration        : (left = $content.attr("duration")|0) != null ? left : null, // ついてない時がある
-                    title           : (left1 = $content.attr("title")) != null ? left1 : null,      // ついてない時がある
-                    content         : $content.text()
-                }})
-        },
+      contents: _.map($stream.find("contents_list contents"), function (el) {
+        let left, left1;
+        const $content = Cheerio(el);
+        return {
+          id: $content.attr("id"),
+          startTime: new Date(($content.attr("start_time") | 0) * 1000),
+          disableAudio: ($content.attr("disableAudio") | 0) === 1,
+          disableVideo: ($content.attr("disableVideo") | 0) === 1,
+          duration:
+            (left = $content.attr("duration") | 0) != null ? left : null, // ついてない時がある
+          title: (left1 = $content.attr("title")) != null ? left1 : null, // ついてない時がある
+          content: $content.text(),
+        };
+      }),
+    },
 
-        // 放送者情報
-        owner   : {
-            userId      : $stream.find("owner_id").text()|0,
-            name        : $stream.find("owner_name").text()
-        },
+    // 放送者情報
+    owner: {
+      userId: $stream.find("owner_id").text() | 0,
+      name: $stream.find("owner_name").text(),
+    },
 
-        // ユーザー情報
-        user    : {
-            id          : $user.find("user_id").text()|0,
-            name        : $user.find("nickname").text(),
-            isPremium   : $user.find("is_premium").text() === "1"
-        },
+    // ユーザー情報
+    user: {
+      id: $user.find("user_id").text() | 0,
+      name: $user.find("nickname").text(),
+      isPremium: $user.find("is_premium").text() === "1",
+    },
 
-        // RTMP情報
-        rtmp    : {
-            isFms       : $rtmp.attr("is_fms") === "1",
-            port        : $rtmp.attr("rtmpt_port")|0,
-            url         : $rtmp.find("url").text(),
-            ticket      : $rtmp.find("ticket").text()
-        },
+    // RTMP情報
+    rtmp: {
+      isFms: $rtmp.attr("is_fms") === "1",
+      port: $rtmp.attr("rtmpt_port") | 0,
+      url: $rtmp.find("url").text(),
+      ticket: $rtmp.find("ticket").text(),
+    },
 
-        // コメントサーバー情報
-        comment : {
-            addr        : $ms.find("addr").text(),
-            port        : $ms.find("port").text()|0,
-            thread      : $ms.find("thread").text()|0
-        },
+    // コメントサーバー情報
+    comment: {
+      addr: $ms.find("addr").text(),
+      port: $ms.find("port").text() | 0,
+      thread: $ms.find("thread").text() | 0,
+    },
 
-        // _hasError: $res("getplayerstatus").attr("status") !== "ok"
-    })
+    // _hasError: $res("getplayerstatus").attr("status") !== "ok"
+  });
 
-    return props
-}
+  return props;
+};
